46f9cd78108090bfa8ed6638fd4f086bb22a79c3
Vedant Chauhan <staranonymous1011@gmail.com>
2026-02-10 14:51:58 +0530

    oops Banner App

---
+++ OopsBannerApp.java
@@ -0,0 +1,30 @@
+public class OopsBannerApp {
+
+    static void printO() {
+        System.out.println("****");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println("****");
+    }
+
+    static void printP() {
+        System.out.println("****");
+        System.out.println("*  *");
+        System.out.println("****");
+        System.out.println("*   ");
+    }
+
+    static void printS() {
+        System.out.println("****");
+        System.out.println("*   ");
+        System.out.println("   *");
+        System.out.println("****");
+    }
+
+    public static void main(String[] args) {
+        printO();
+        printO();
+        printP();
+        printS();
+    }
+}
